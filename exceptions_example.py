e2a8269b8acec31778a6202d8d4b88ee0fbd1959
Kian <kianluis1382@gmail.com>
2025-01-22 17:53:42 +0100

    last changes saved

--- exceptions_example.py
+++ exceptions_example.py
@@ -1,4 +1,20 @@
 name = input("what is your name?")
 print("hello", name)
 age = input("How old are you?")
-print("you were probably born in", 2024-int(age))
+try:
+    age = int(age)  # I am trying to convert it to a number
+    print("you were probably born in", 2024 - age)
+    new_age = age / 0
+except ValueError:
+    print("you are trying to trick me")
+    print("better luck next time")
+except ZeroDivisionError:
+    print("you cant divide by zero")
+except:
+    print("something unexpected happened")
+else: # this happens if no error occurs
+    print("you were probably born in", 2024 - age)
+finally:
+    print("thanks for playing")
+
+
